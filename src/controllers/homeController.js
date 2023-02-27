1ba16181172dc687b45a8d5fc1b5a03f80edce50
Tri Nguyen <nguyendinhtri@gmail.com>
2023-02-27 12:41:00 +0700

    Create User service.js

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,6 @@
 
 import db from '../models/index';
+import CRUDService from '../services/CRUDService';
 
 let getHomePage = async (req, res) => {
     try {
@@ -13,6 +14,18 @@ let getHomePage = async (req, res) => {
 
 };
 
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+};
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send("post crud from server");
+};
+
 module.exports = {
     getHomePage: getHomePage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 };
